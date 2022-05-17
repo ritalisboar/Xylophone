690d21b7dd7cad5e9cf26d00567c131d7020004c
Rita Lisboa <cassialisboar@gmail.com>
2022-05-16 21:52:52 -0300

    commit edit buttons

--- Xylophone/XylophoneButtons.swift
+++ Xylophone/XylophoneButtons.swift
@@ -39,7 +39,7 @@ class XylophoneButtons: UIView {
         cButton.topAnchor.constraint(equalTo: topAnchor, constant: 55).isActive = true
         cButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 5).isActive = true
         cButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5).isActive = true
-        cButton.heightAnchor.constraint(equalToConstant: 110).isActive = true
+        cButton.heightAnchor.constraint(equalToConstant: 105).isActive = true
     }
     
     
@@ -56,9 +56,9 @@ class XylophoneButtons: UIView {
     
     private func constraintsDButton() {
         dButton.topAnchor.constraint(equalTo: cButton.bottomAnchor, constant: 7).isActive = true
-        dButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15).isActive = true
-        dButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15).isActive = true
-        dButton.heightAnchor.constraint(equalToConstant: 110).isActive = true
+        dButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10).isActive = true
+        dButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
+        dButton.heightAnchor.constraint(equalToConstant: 105).isActive = true
     }
     
     // MARK: - eButton
@@ -74,9 +74,9 @@ class XylophoneButtons: UIView {
     
     private func constraintsEButton() {
         eButton.topAnchor.constraint(equalTo: dButton.bottomAnchor, constant: 7).isActive = true
-        eButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25).isActive = true
-        eButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -25).isActive = true
-        eButton.heightAnchor.constraint(equalToConstant: 110).isActive = true
+        eButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15).isActive = true
+        eButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15).isActive = true
+        eButton.heightAnchor.constraint(equalToConstant: 105).isActive = true
     }
     
     // MARK: - fButton
@@ -92,9 +92,9 @@ class XylophoneButtons: UIView {
     
     private func constraintsFButton() {
         fButton.topAnchor.constraint(equalTo: eButton.bottomAnchor, constant: 7).isActive = true
-        fButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 35).isActive = true
-        fButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -35).isActive = true
-        fButton.heightAnchor.constraint(equalToConstant: 110).isActive = true
+        fButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20).isActive = true
+        fButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20).isActive = true
+        fButton.heightAnchor.constraint(equalToConstant: 105).isActive = true
     }
     
     // MARK: - gButton
@@ -110,9 +110,9 @@ class XylophoneButtons: UIView {
     
     private func constraintsGButton() {
         gButton.topAnchor.constraint(equalTo: fButton.bottomAnchor, constant: 7).isActive = true
-        gButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 45).isActive = true
-        gButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -45).isActive = true
-        gButton.heightAnchor.constraint(equalToConstant: 110).isActive = true
+        gButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25).isActive = true
+        gButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -25).isActive = true
+        gButton.heightAnchor.constraint(equalToConstant: 105).isActive = true
     }
     
     // MARK: - aButton
@@ -128,9 +128,9 @@ class XylophoneButtons: UIView {
     
     private func constraintsAButton() {
         aButton.topAnchor.constraint(equalTo: gButton.bottomAnchor, constant: 7).isActive = true
-        aButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 55).isActive = true
-        aButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -55).isActive = true
-        aButton.heightAnchor.constraint(equalToConstant: 110).isActive = true
+        aButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 30).isActive = true
+        aButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -30).isActive = true
+        aButton.heightAnchor.constraint(equalToConstant: 105).isActive = true
     }
     
     
@@ -147,9 +147,9 @@ class XylophoneButtons: UIView {
     
     private func constraintsBButton() {
         bButton.topAnchor.constraint(equalTo: aButton.bottomAnchor, constant: 7).isActive = true
-        bButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 65).isActive = true
-        bButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -65).isActive = true
-        bButton.heightAnchor.constraint(equalToConstant: 110).isActive = true
+        bButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 35).isActive = true
+        bButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -35).isActive = true
+        bButton.heightAnchor.constraint(equalToConstant: 105).isActive = true
     }
     
     override init(frame: CGRect) {
